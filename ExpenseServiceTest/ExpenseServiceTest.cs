ceb83dffb12e95184ed503532efbcf9e951ecf74
Yixi <lyh1112@gmail.com>
2023-10-13 16:42:02 +0800

    feat: add test throw unexpected project exception when project is invalid

--- ExpenseServiceTest/ExpenseServiceTest.cs
+++ ExpenseServiceTest/ExpenseServiceTest.cs
@@ -1,4 +1,5 @@
 using Expense.Service;
+using Expense.Service.Exceptions;
 using Expense.Service.Expense;
 using Expense.Service.Projects;
 using Xunit;
@@ -55,8 +56,11 @@ namespace Expense.Service.Test
         public void Should_throw_unexpected_project_exception_if_project_is_invalid()
         {
             // given
+            Project project = new Project(ProjectType.UNEXPECTED_PROJECT_TYPE, "Project C");
             // when
+            var exception = Assert.Throws<UnexpectedProjectTypeException>(() => ExpenseService.GetExpenseCodeByProjectTypeAndName(project));
             // then
+            Assert.Equal("You enter invalid project type", exception.Message);
         }
     }
 }
