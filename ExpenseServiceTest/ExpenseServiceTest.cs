bffed03712dd5ecb997aaac0c31a29c865655b6f
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-13 18:11:36 +0800

    test: Should_return_expense_type_B_if_project_is_external_and_name_is_project_B

--- ExpenseServiceTest/ExpenseServiceTest.cs
+++ ExpenseServiceTest/ExpenseServiceTest.cs
@@ -32,8 +32,11 @@ namespace Expense.Service.Test
         public void Should_return_expense_type_B_if_project_is_external_and_name_is_project_B()
         {
             // given
+            Project projectB = new Project(ProjectType.EXTERNAL, "Project B");
             // when
+            ExpenseType expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(projectB);
             // then
+            Assert.Equal(ExpenseType.EXPENSE_TYPE_B, expenseType);
         }
 
         [Fact]
