d0d60794e3852a232b47a93d3396fc9bfd84b7de
yoogodong <yoogdong@gmail.com>
2023-10-17 12:48:10 +0800

    test: first

--- ExpenseServiceTest/ExpenseServiceTest.cs
+++ ExpenseServiceTest/ExpenseServiceTest.cs
@@ -1,4 +1,7 @@
 using Expense.Service;
+using Expense.Service.Exceptions;
+using Expense.Service.Expense;
+using Expense.Service.Projects;
 using Xunit;
 
 namespace Expense.Service.Test
@@ -9,40 +12,13 @@ namespace Expense.Service.Test
         public void Should_return_internal_expense_type_if_project_is_internal()
         {
             // given
-            // when
-            // then
-        }
+            Project project = new Project(ProjectType.INTERNAL, "first");
 
-        [Fact]
-        public void Should_return_expense_type_A_if_project_is_external_and_name_is_project_A()
-        {
-            // given
             // when
-            // then
-        }
+            Expense.ExpenseType expenseType = ExpenseService.GetExpenseCodeByProjectTypeAndName(project);
 
-        [Fact]
-        public void Should_return_expense_type_B_if_project_is_external_and_name_is_project_B()
-        {
-            // given
-            // when
             // then
-        }
-
-        [Fact]
-        public void Should_return_other_expense_type_if_project_is_external_and_has_other_name()
-        {
-            // given
-            // when
-            // then
-        }
-
-        [Fact]
-        public void Should_throw_unexpected_project_exception_if_project_is_invalid()
-        {
-            // given
-            // when
-            // then
+            Assert.Equal(ExpenseType.INTERNAL_PROJECT_EXPENSE, expenseType);
         }
     }
 }
